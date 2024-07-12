b7757f7d5c39b81bb9fecc02f95d019ebcee1cf3
Sathish. G <sadie26032005@gmail.com>
2024-07-12 23:45:41 +0530

    Model has been created and all files has been added

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -6,60 +6,75 @@ import numpy as np
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import LabelEncoder,StandardScaler
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import PCA
+from sklearn.base import TransformerMixin, BaseEstimator
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object
+
 
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path = os.path.join('models','preprocessor.pkl')
 
+class DenseTransformer(TransformerMixin, BaseEstimator):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return X.toarray()
 
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config = DataTransformationConfig()
 
     def get_data_transformation_object(self):
-        try :
-            numerical_columns = ['votes','approx_cost(for two people)']
-            categorical_columns = ['online_order','book_table','rest_type','cuisines','listed_in(type)','listed_in(city)']
-            pca_cols = ['dish_liked']
-            num_pipeline = Pipeline(
-                steps = [
-                    ('imputer',SimpleImputer(strategy='median')),
-                    ('scaler',StandardScaler())
-                ]
-            )
-            cat_pipeline = Pipeline(
-                steps=[
-                    ('imputer',SimpleImputer(strategy='most_frequent')),
-                    ('label_encoder',LabelEncoder()),
-                    ('scaler',StandardScaler())
-                ]
-            )
-            pca_pipeline = Pipeline([
-                    ('tfidf', TfidfVectorizer()), 
-                    ('pca', PCA(n_components=1))
-            ])
-            logging.info('Numerical and Categorical Columns and PCA are converted into the needed format and standardized it .')
+        try:
+            numerical_columns = ['votes', 'approx_cost(for two people)']
+            categorical_columns = ['online_order', 'book_table', 'rest_type', 'cuisines', 'listed_in(type)', 'listed_in(city)']
+            pca_cols = 'dish_liked'
 
+            num_pipeline = Pipeline(
+                steps=[
+                    ('imputer', SimpleImputer(strategy='median')),
+                    ('scaler', StandardScaler())
+                ]
+            )
+
+            cat_pipeline = Pipeline(
+            steps=[
+                ('imputer', SimpleImputer(strategy='most_frequent')),
+                ('ordinal', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=413733)),
+                ('scaler', StandardScaler())
+            ]
+            )
+            svd_pca_pipeline = Pipeline(
+            steps=[
+                ('tfidf', TfidfVectorizer()),
+                ('to_dense', DenseTransformer()), 
+                ('pca', PCA(n_components=1))
+            ]
+            )
+
+
+            logging.info('Numerical, Categorical, and PCA Columns are transformed and standardized.')
 
             preprocessor = ColumnTransformer(
-                transformers=[
-                    ('num_pipeline',num_pipeline,numerical_columns),
-                    ('cat_pipeline',cat_pipeline,categorical_columns),
-                    ('pca_pipeline',pca_pipeline,pca_cols)
-                ]
+            transformers=[
+                ('num_pipeline', num_pipeline, numerical_columns),
+                ('cat_pipeline', cat_pipeline, categorical_columns),
+                ('pca_pipeline', svd_pca_pipeline, pca_cols)
+            ]
             )
+
             return preprocessor
         except Exception as e:
             raise CustomException(e,sys)
         
 
-    def intiate_data_transformation(self,train_path,test_path):
+    def intiate_data_transformationw(self,train_path,test_path):
         try :
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
@@ -71,28 +86,26 @@ class DataTransformation:
             target_column = 'rate'
             
 
-            input_feature_train_df = train_df.drop(columns=[target_column],axis=1)
+            input_feature_train_df = train_df.drop(columns=[target_column])
             target_feature_train_df = train_df[target_column]
 
-            input_feature_test_df = test_df.drop(columns=[target_column],axis=1)
+            input_feature_test_df = test_df.drop(columns=[target_column])
             target_feature_test_df = test_df[target_column]
             logging.info('Applying data transformation  to the objects.')
-
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
             logging.info('Data transformation has been completed .')
             train_arr = np.c_[input_feature_train_arr,target_feature_train_df]
             test_arr = np.c_[input_feature_test_arr,target_feature_test_df]
-
             save_object(
-                filepath=self.data_transformation_config.preprecessor_obj_file_path,
+                filepath=self.data_transformation_config.preprocessor_obj_file_path,
                 obj = preprocessing_obj
             )
 
             return (
                 train_arr,
                 test_arr,
-                self.data_transformation_config.preprecessor_obj_file_path
+                self.data_transformation_config.preprocessor_obj_file_path
             )
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,14 @@
 import os
-import pickle
-from src.exception import CustomException
 import sys
+import numpy as np
+import pickle
+import pandas as pd
+from logger import logging
+from src.exception import CustomException
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+
 
 
 def save_object(filepath,obj):
@@ -11,5 +18,46 @@ def save_object(filepath,obj):
         with open(filepath,'wb') as f:
             pickle.dump(obj,f)
 
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+        logging.info('Training the model and find its hyperpararameters..')
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+
+
+def load_object(filepath):
+    try:
+        with open(filepath,'rb') as f:
+            return pickle.load(f)
     except Exception as e:
         raise CustomException(e,sys)
